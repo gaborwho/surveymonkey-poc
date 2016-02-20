e08efb857fa690a2a7c586f7993c98b90c48fc15
Gabor Balla <cavalierrant@gmail.com>
2016-02-20 02:27:47 +0100

    add response retrieval

--- read.js
+++ read.js
@@ -7,25 +7,27 @@ var Promise = require('bluebird');
  
 var apiKey = process.env.API_KEY;
 var accessToken = process.env.ACCESS_TOKEN
- 
-try { 
-    var api = Promise.promisifyAll(new SurveyMonkeyAPI(apiKey, accessToken, { version : 'v2', secure : true }));
-} catch (error) {
-    console.log(error.message);
-    process.exit();
-}
 
+var api;
+var surveyId;
  
 Promise.resolve()
+  .then(function () {
+    api = Promise.promisifyAll(new SurveyMonkeyAPI(apiKey, accessToken, { version : 'v2', secure : true }));
+  })
   .then(function () {
     return api.getSurveyListAsync({});
   })
   .then(function (data) {
-    var surveyId = data.data.surveys[0].survey_id;
+    surveyId = data.data.surveys[0].survey_id;
     return api.getRespondentListAsync({ survey_id: surveyId, order_by: 'date_modified', order_asc: true, page_size: 25 })
   })
   .then(function (data) {
-    console.log(data.data.respondents[0].respondent_id);
+    var respondentId = data.data.respondents[0].respondent_id;
+    return api.getResponsesAsync({ respondent_ids: [respondentId], survey_id: surveyId, order_asc: true, page_size: 25 })
+  })
+  .then(function (data) {
+    console.log(JSON.stringify(data));
   })
   .catch(function(error) {
     console.log(error.message);
