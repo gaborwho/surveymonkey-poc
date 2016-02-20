05cfd1ae68bdee557c87ae28649352dcbe84b1e3
Gabor Balla <cavalierrant@gmail.com>
2016-02-20 02:34:23 +0100

    refactor to generator method using co

--- read.js
+++ read.js
@@ -2,34 +2,34 @@
 
 require('dotenv').config();
 
-var SurveyMonkeyAPI = require('surveymonkey').SurveyMonkeyAPI;
-var Promise = require('bluebird');
- 
-var apiKey = process.env.API_KEY;
-var accessToken = process.env.ACCESS_TOKEN
+const SurveyMonkeyAPI = require('surveymonkey').SurveyMonkeyAPI;
+const Promise = require('bluebird');
+const co = require('co');
 
-var api;
-var surveyId;
- 
-Promise.resolve()
-  .then(function () {
-    api = Promise.promisifyAll(new SurveyMonkeyAPI(apiKey, accessToken, { version : 'v2', secure : true }));
-  })
-  .then(function () {
-    return api.getSurveyListAsync({});
-  })
-  .then(function (data) {
-    surveyId = data.data.surveys[0].survey_id;
-    return api.getRespondentListAsync({ survey_id: surveyId, order_by: 'date_modified', order_asc: true, page_size: 25 })
-  })
-  .then(function (data) {
-    var respondentId = data.data.respondents[0].respondent_id;
-    return api.getResponsesAsync({ respondent_ids: [respondentId], survey_id: surveyId, order_asc: true, page_size: 25 })
-  })
-  .then(function (data) {
-    console.log(JSON.stringify(data));
-  })
-  .catch(function(error) {
+const apiKey = process.env.API_KEY;
+const accessToken = process.env.ACCESS_TOKEN
+
+const apiOptions = { version : 'v2', secure : true };
+
+co(function*() {
+  try {
+    const api = Promise.promisifyAll(new SurveyMonkeyAPI(apiKey, accessToken, apiOptions));
+
+    const surveyList = yield api.getSurveyListAsync({});
+    const surveyId = surveyList.data.surveys[0].survey_id;
+
+    const respondentList = yield api.getRespondentListAsync({
+      survey_id: surveyId,
+      order_by: 'date_modified', order_asc: true,
+      page_size: 25
+    });
+
+    const respondentId = respondentList.data.respondents[0].respondent_id;
+    const responses = yield api.getResponsesAsync({ respondent_ids: [respondentId], survey_id: surveyId, order_asc: true, page_size: 25 })
+
+    console.log(JSON.stringify(responses));
+  } catch (error) {
     console.log(error.message);
-  });
+  }
+});
 
